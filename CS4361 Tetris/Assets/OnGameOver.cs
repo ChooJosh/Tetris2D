222bdae21c21cf24bd6a83beee3a536649d3f57c
weruder <myncijub@gmail.com>
2019-11-18 21:02:44 -0600

    Added game over menu

---
+++ CS4361 Tetris/Assets/OnGameOver.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnGameOver : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public static GameObject gameOverObject;
+
+    void Start()
+    {
+        gameOverObject = GameObject.FindGameObjectsWithTag("GameOverCanvas")[0];
+        gameOverObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public static void SetGameOver()
+    {
+        gameOverObject.SetActive(true);
+    }
+}
